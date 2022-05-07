205b4cb865bdb086af59cc10a864bd57d3707dfb
Dan <daniel2.millward@live.uwe.ac.uk>
2022-05-07 18:45:26 +0100

    Initial Commit

---
+++ Trainers/TrainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainersApp.swift
+//  Trainers
+//
+//  Created by Daniel Millward on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
